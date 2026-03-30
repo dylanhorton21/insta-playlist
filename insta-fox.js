e8b80a699e8faa554a724a29225b4800923ce7d1
dylanhorton21 <dzh5661@psu.edu>
2026-03-29 21:57:52 -0400

    check in 2, local storage and json, need API and routing

--- insta-fox.js
+++ insta-fox.js
@@ -10,23 +10,60 @@ export class InstaFox extends DDDSuper(LitElement){
         return "insta-fox";
     }
     static properties = {
-        image: {type: String},
+        photos: {type: Array},
         loading: {type: Boolean},
+        activeIndex: {type: Number},
+        likes: {type: Object},
         };
         constructor(){
             super();
-            this.image = "";
+            this.photos = [];
             this.loading = true;
+            this.activeIndex = 0;
+            this.likes = {};
         }
         connectedCallback(){
             super.connectedCallback();
-            this.fox();
+            this.loadData();
+            this.loadFromStorage();
         }
-        async fox(){
-            const res = await fetch("https://randomfox.ca/floof/");
+        async loadData(){
+            const url = new URL("./data.json", import.meta.url).href;
+            const res = await fetch(url);
             const data = await res.json();
-            this.image = data.image;
+            this.photos = data.photos;
             this.loading = false;
+        }
+        nextPhoto(){
+            if(this.activeIndex < this.photos.length - 1){
+                this.activeIndex++;
+            }
+        }
+        previousPhoto(){
+            if(this.activeIndex > 0){
+                this.activeIndex--;
+            }
+        }
+        dotPhoto(index){
+            this.activeIndex = index;
+        }
+        saveStorage(){
+            localStorage.setItem("likes", JSON.stringify(this.likes));
+        }
+        loadFromStorage(){
+            const savedLikes = localStorage.getItem("likes");
+            if(savedLikes){
+                this.likes = JSON.parse(savedLikes);
+            }
+        }
+        toggleLike(id){
+            if(this.likes[id]){
+                delete this.likes[id];        
+            } else {
+                this.likes[id] = true;
+            }
+            this.likes = {...this.likes};
+            this.saveStorage();
         }
         static styles = css`
         .card{
@@ -53,23 +90,87 @@ export class InstaFox extends DDDSuper(LitElement){
         .caption {
             padding: 10px;
         }
+        .navigation{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 10px;
+        
+        }
+        button{
+            border: 1px solid black;
+            border-radius: 8px;
+            background: var(--ddd-theme-default-white);
+            cursor: pointer;
+        }
+        .allDots{
+            display: flex;
+            justify-content: center;
+            padding: 10px;
+            gap: 5px;
+        }
+        .dot{
+            cursor: pointer;
+            font-size: 14px;
+            color: var(--ddd-theme-default-black);
+            opacity: .25;
+        }
+        .dot.active{
+            color: var(--ddd-theme-default-black);
+            opacity: 1;
+
+        }
+        .heart{
+           cursor:pointer;
+        }
         `;
         render(){
             
             if(this.loading){
                 return html`<p>This loads a fox!!!</p>`;
             }
+            const photo = this.photos[this.activeIndex];
             return html`
             <div class = "card">
                 <div class="header">
                 <div class= "appname">InstaFox</div>
                 </div>
-                <img src = "${this.image}" loading="lazy" />
+                <img src = "${photo.thumbnail}" alt="${photo.name}" loading="lazy" />
                 <div class = "caption">
-                    ♡  💬  Random Fox Refresh!!
+                    <span class="heart" @click="${() =>this.toggleLike(photo.id)}">
+                        ${this.likes[photo.id] ? "♥︎" : "♡"}
+                    </span>
+                    ${photo.name}
+                    <div>
+                    Fox photos from today
+                    </div>
+                      
+                </div>
+                <div class = "navigation">
+                    <button @click= "${this.previousPhoto}" ?disabled = "${this.activeIndex === 0}">
+                        <
+                    </button>
+
+                    
+                    <button @click="${this.nextPhoto}" ?disabled = "${this.activeIndex === this.photos.length - 1}">
+                        >
+                    </button>
+                </div>
+                <div class = "allDots">
+                    ${this.photos.map(
+                    (_,index) => html`
+                    <span
+                    class="dot ${this.activeIndex === index ? "active" : ""}"
+                    @click = "${() => this.dotPhoto(index)}"
+                    >
+                    ●
+                </span>
+                    `    
+                    )}
                 </div>
             </div>
-            `;
+            `
+            ;
         }
     }
     customElements.define(InstaFox.tag, InstaFox);
